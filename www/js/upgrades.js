5bce48dd66eb172c6974a87898967002952c6e8e
rekarnar <rekarnar@gmail.com>
2014-03-27 14:37:33 +1300

    adding upgrades info file

---
+++ www/js/upgrades.js
@@ -0,0 +1,15 @@
+var upgrades =  {
+    'upgrade1': {
+        sprite: 'upgrade1',
+        price: 2,
+        size: 30,
+        fire_rate: 120
+    },
+    'upgrade2': {
+        sprite: 'upgrade2',
+        price: 4,
+        size: 30,
+        fire_rate: 120
+    },
+
+};
