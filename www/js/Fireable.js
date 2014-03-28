410d691a0937e140cc9a87005502f6cc9fcef13b
rekarnar <rekarnar@gmail.com>
2014-03-28 13:13:37 +1300

    adding new classes

---
+++ www/js/Fireable.js
@@ -0,0 +1,9 @@
+var Fireable = function () {
+    awesome: 'yer'
+}
+
+Fireable.prototype.fireRate = 1000;
+Fireable.prototype._fireCooldown = 0; // internal counter to work with fireRate
+Fireable.prototype.fireLimit = 100;
+Fireable.prototype.fireDisable = false; // if over an upgrade etc
+Fireable.prototype.fireAmount = 1;
